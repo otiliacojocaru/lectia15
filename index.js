2a1a40b858feafe2e43eeef12c0fddab65b638a2
otiliacojocaru <otilicacojocaru0708@gmail.com>
2024-10-12 15:09:41 +0300

    lectia15

---
+++ index.js
@@ -0,0 +1,81 @@
+// const camelCase = {};
+// const CONSTANTE = 'denumirea';
+// const SNAKE_CASE = 'text';
+// const Capital = 'test';
+// const kebab_case = 'pentru denumire de clase';
+
+// const NAME = 'iulian'
+
+//console.log('my name ' + ':' + ' ' + 'iulian')
+
+
+//console.log('my name is: ${NAME} AND I USE ${SNAKE_CASE}')
+
+//const str = '6'
+
+//console.log(typeof Number(str), Number(str))
+        //ori
+
+//  console.log(typeof +str, str)      
+//  console.log(3+ +str) 
+
+// console.log('don\'t go there')
+
+
+// const num = 1;
+
+// console.log(typeof num.toString())
+
+// const str = 'str';
+
+// const condition = false;
+
+// if(typeof str === 'number' ) {
+//         console.log('pass',typeof str)
+// } else {
+//         console.log('not pass')
+// }
+
+// if(3>1) {
+//         console.log('pass')
+// } else{
+//         console.log('not pass')
+// }
+
+// if(3 >= 1) {
+//         console.log('pass')
+// } else{
+//         console.log('not pass')
+// }
+
+// if(3 !== 1){
+//         console.log('pass')
+// } else if(3>2){
+//         console.log('not pass')
+// }
+
+
+//  || este or
+// const condition1 = true;
+// const condition2 = false;
+
+// if (condition2 || condition1 ){
+//         console.log('pass')
+// }
+
+// const defaultname = 'user';
+// const username = 'john';
+
+// const usedname = username || defaultname;
+
+// console.log(usedname)
+
+// console.log(true && false) //&& and
+
+// const condition = false;
+// const condition2 = false;
+// const condition3 = true;
+// const ternary = condition ? 'option1' : condition2 ? 'option2' : condition3 ? 'option3' : 'option4';
+
+// console.log(ternary)
+
